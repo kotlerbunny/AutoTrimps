c695d8b7da759229f9890709d290bf8d2d4f9562
kotlerbunny <92127741+kotlerbunny@users.noreply.github.com>
2021-10-08 02:21:03 +0300

    Update .user.js
    script install file

--- .user.js
+++ .user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         AutoTrimps-Ray
 // @version      6.0.0.0
-// @namespace    https://Psycho-Ray.github.io/AutoTrimps
-// @downloadURL  https://Psycho-Ray.github.io/AutoTrimps/.user.js
-// @updateURL    https://Psycho-Ray.github.io/AutoTrimps/.user.js
+// @namespace    https://kotlerbunny.github.io/AutoTrimps
+// @downloadURL  https://kotlerbunny.github.io/AutoTrimps/.user.js
+// @updateURL    https://kotlerbunny.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, Psycho-Ray
 // @include      *trimps.github.io*
@@ -17,7 +17,7 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-Ray';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://Psycho-Ray.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://kotlerbunny.github.io/AutoTrimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
